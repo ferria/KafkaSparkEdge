52d5afa2401bf1eb1051b1a18b429f3ebd8f086f
akaanirban <anirban.dash@gmail.com>
2018-02-08 17:34:50 -0500

    WIP

--- Docker/buildDocker.sh
+++ Docker/buildDocker.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-
 NODE_BASE_IMAGE_NAME=anirbandas/kafkaspark_edge
 NODE_BASE_VERSION="0.0.1"
-
 
 function buildImage() {
     #VERSIONED_IMAGE="$NODE_BASE_IMAGE_NAME:$NODE_BASE_VERSION"
     #docker build -t $VERSIONED_IMAGE -f Dockerfile . 
     docker build -t $NODE_BASE_IMAGE_NAME -f dockerfile .
-    docker tag $VERSIONED_IMAGE $NODE_BASE_IMAGE_NAME:latest && echo "Tagged image...Pushing to Dockerhub"
+    docker tag $NODE_BASE_IMAGE_NAME $NODE_BASE_IMAGE_NAME:latest 
+    echo "Tagged image...Pushing to Dockerhub"
     docker push $NODE_BASE_IMAGE_NAME:latest
 }
+
 echo "Building docker image..."
 buildImage
 echo "Build Done...Push Complete..."
+
--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Starting ZooKeeper and Kafka in background at port 2181 and 9092"
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
