701457687a953e63af0f7cb9fb1f5c2ffa8489ce
Anthony Ferritto <ferria@users.noreply.github.com>
2018-02-11 19:34:25 -0500

    Update wc.py

--- wc.py
+++ wc.py
@@ -10,7 +10,7 @@ if __name__ == “__main__”:
     broker, topic = sys.argv[1:]
     kvs = KafkaUtils.createStream(ssc, \
                                   broker, \
-                                  “raw-event-streaming-consumer”,\{topic:1}) 
+                                  “raw-event-streaming-consumer”,{topic:1}) 
     lines = kvs.map(lambda x: x[1])
     counts = lines.flatMap(lambda line: line.split(“ “)) 
                   .map(lambda word: (word, 1)) \
